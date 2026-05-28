d0eee969911fccd453e9412dcbe615e853ad9cff
bryanMFA <155461299+bryanMFA@users.noreply.github.com>
2026-05-28 21:40:17 +0200

    what route to laod?

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,9 +24,9 @@ const athletes = [
 ];
 
 // For very small static site, data may be embedded on the page.
-// Otherwise load it from `assets/data.json` and then render.
+// Otherwise load it from `./assets/data.json` and then render.
 function loadData() {
-  return fetch('assets/data.json')
+  return fetch('./assets/data.json')
     .then(r => {
       if (!r.ok) throw new Error('Failed to load data.json ' + r.status);
       return r.json();
